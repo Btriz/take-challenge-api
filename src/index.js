5257c42012190e2a33396a5cb8dbaf3fd36ae019
btriz <00btrz@gmail.com>
2021-12-14 18:19:24 -0300

    config heroku port

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extend: true }));
 app.use(require('./routes'));
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('On');
 });
